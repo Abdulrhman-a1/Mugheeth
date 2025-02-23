44e190ced9e77bad33aaa07ce54a702c026e77f2
Abdulrhman-a1 <Abdulrhman-a1@outlook.com>
2025-02-23 20:20:00 +0300

    Please enter the commit message for your changes. Lines starting

--- lib/features/init_dependencies.dart
+++ lib/features/init_dependencies.dart
@@ -3,6 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:get_it/get_it.dart';
 import '../data/auth/bloc/auth_bloc.dart';
 import '../data/auth/data/datasources/auth_data_source.dart';
+import '../data/auth/domain/usecases/current_user.dart';
 import '../data/auth/domain/usecases/user_sign_in.dart';
 import '../data/auth/domain/usecases/user_sign_up.dart';
 import '../data/auth/data/repo/auth_repo_impl.dart';
@@ -21,34 +22,37 @@ Future<void> initDependencies() async {
 }
 
 void _initAuth() {
-  serviceLocator.registerFactory<AuthDataSource>(
-    () => AuthDataSourceImpl(
-      serviceLocator(),
-    ),
-  );
-
-  serviceLocator.registerFactory<AuthRepo>(
-    () => AuthRepoImpl(
-      serviceLocator(),
-    ),
-  );
-
-  serviceLocator.registerFactory(
-    () => UserSignUp(
-      serviceLocator(),
-    ),
-  );
-
-  serviceLocator.registerFactory(
-    () => UserSignIn(
-      serviceLocator(),
-    ),
-  );
-
-  serviceLocator.registerLazySingleton(
-    () => AuthBloc(
-      userSignUp: serviceLocator(),
-      userSignIn: serviceLocator(),
-    ),
-  );
+  serviceLocator
+    ..registerFactory<AuthDataSource>(
+      () => AuthDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory<AuthRepo>(
+      () => AuthRepoImpl(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => UserSignUp(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => UserSignIn(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => CurrentUser(
+        serviceLocator(),
+      ),
+    )
+    ..registerLazySingleton(
+      () => AuthBloc(
+        currentUser: serviceLocator(),
+        userSignUp: serviceLocator(),
+        userSignIn: serviceLocator(),
+      ),
+    );
 }
